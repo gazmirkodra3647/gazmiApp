cba96055a0d4259d0ee21b3c27cb38bf9ac3a391
gazmirkodra3647 <146691269+gazmirkodra3647@users.noreply.github.com>
2025-07-14 09:22:34 +0200

    Create usePrevious.ts

---
+++ ui/hooks/usePrevious.ts
@@ -0,0 +1,8 @@
+import { useRef } from 'react';
+
+export function usePrevious<T>(value: T): T | undefined {
+  const ref = useRef<T | undefined>();
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+  return ref.current;
