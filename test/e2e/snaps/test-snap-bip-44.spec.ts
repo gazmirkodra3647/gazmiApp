ecc1c1ab84aab1663adbf82204460a627c192f61
gazmirkodra3647 <146691269+gazmirkodra3647@users.noreply.github.com>
2025-07-25 06:32:45 +0200

    Create test-snap-bip-44.spec.ts

---
+++ test/e2e/snaps/test-snap-bip-44.spec.ts
@@ -0,0 +1,7 @@
+import { TestSnaps } from '../page-objects/pages/test-snaps';
+import Driver from '../webdriver/driver';
+import loginWithBalanceValidation from '../page-objects/flows/login.flow';
+import FixtureBuilder from '../fixture-builder';
+import withFixtures from '../helpers';
+import switchAndApproveDialogSwitchToTestSnap from '../page-objects/flows/snap-permission.flow';
+import openTestSnapClickButtonAndInstall from '
